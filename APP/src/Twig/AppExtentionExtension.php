017b9d2864b09993c988b54003a8fb3a2ea6829c
M-HASSANEIN <momed999@hotmail.com>
2022-01-25 18:29:29 +0100

    update

--- APP/src/Twig/AppExtentionExtension.php
+++ APP/src/Twig/AppExtentionExtension.php
@@ -37,6 +37,6 @@ class AppExtentionExtension extends AbstractExtension
     }
      public function koko()
     {
-        return "tiko web site new goooood";
+        return "tiko web site new new  goooood";
     }
 }
